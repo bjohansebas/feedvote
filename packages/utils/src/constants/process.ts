e720accd3c5a9f9e60d8abce35a52fdd38f4fc57
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-16 20:46:06 -0500

    feat(utils): ✨ add constant

---
+++ packages/utils/src/constants/process.ts
@@ -0,0 +1,8 @@
+// Github environment variables
+
+export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID as string
+export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET as string
+
+// Aptabase environment variables
+
+export const APTABASE_APP_KEY = process.env.APTABASE_APP_KEY as string
