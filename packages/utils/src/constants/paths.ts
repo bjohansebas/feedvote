1671a59c4a36ec037263c7ec2155d51b04e35aa2
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-18 21:14:02 -0500

    feat(utils): ✨ add new constants

---
+++ packages/utils/src/constants/paths.ts
@@ -0,0 +1,20 @@
+export const PRIVATE_KEYS = new Set(['dashboard', 'account'])
+
+export const PRIVATE_PATHS = new Set(['/register/workspace'])
+
+export const RESERVED_KEYS = new Set([
+  'docs',
+  'dashboard',
+  'account',
+  'legal',
+  'changelog',
+  'pricing',
+  'register',
+  'login',
+  'dashboard',
+  'account',
+  'help',
+  'blog',
+  'contact',
+  'about',
+])
