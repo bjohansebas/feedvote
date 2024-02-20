d0d8207c51e686230cd3e349ad17b99b206d5e40
Sebastian Beltran <bjohansebas@gmail.com>
2024-02-19 22:06:08 -0500

    feat(utils): ✨ add status code

--- packages/utils/src/constants/index.ts
+++ packages/utils/src/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './process'
 export * from './paths'
+export * from './status-code'
---
+++ packages/utils/src/constants/status-code.ts
@@ -0,0 +1,16 @@
+// 20x codes
+export const OK_CODE = 200
+export const CREATED_CODE = 201
+export const ACCEPTED_CODE = 202
+
+// 40x codes
+export const BAD_REQUEST_CODE = 400
+export const UNAUTHORIZED_CODE = 401
+export const PAYMENT_REQUIRED_CODE = 402
+export const FORBIDDEN_CODE = 403
+export const NOT_FOUND_CODE = 404
+export const CONFLICT_CODE = 409
+
+// 50X codes
+export const INTERNAL_SERVER_ERROR_CODE = 500
+export const NOT_IMPLEMENTED_CODE = 501
