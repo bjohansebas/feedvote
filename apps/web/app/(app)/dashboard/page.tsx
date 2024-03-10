bd605bc2e1ed37ba24b5ba51e0bc0d79bcb9f827
Sebastian Beltran <bjohansebas@gmail.com>
2024-03-09 21:58:25 -0500

    feat(app): improve dashboard

--- apps/web/app/(app)/dashboard/page.tsx
+++ apps/web/app/(app)/dashboard/page.tsx
@@ -1,12 +1,26 @@
+import { Button } from '@feedvote/ui'
 import { getOrganizationUser } from '@lib/api/organization'
 import { CardOrganization } from '@ui/app/dashboard/card-organizations'
 
+import { unstable_cache as cache } from 'next/cache'
+import Link from 'next/link'
+
+const getOrganizations = cache(getOrganizationUser, ['organizations'], {
+  tags: ['organizations'],
+})
+
 export default async function DashboardPage() {
-  const organization = await getOrganizationUser()
+  const organization = await getOrganizations()
 
   return (
-    <main className="mx-auto mt-10 max-w-5xl px-5">
-      <div className="flex w-full flex-col">
+    <main className="mx-auto mt-10 max-w-5xl space-y-5 px-5 pb-20">
+      <header className="flex flex-wrap items-center justify-between gap-y-4">
+        <h2 className="font-semibold text-xl">My organizations</h2>
+        <Button size="sm" variant="outline" asChild>
+          <Link href="/register/organization">Create new organization</Link>
+        </Button>
+      </header>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(330px,1fr))] gap-4">
         {organization.data?.map(({ logo, name, slug, id }) => (
           <CardOrganization logo={logo} name={name} slug={slug} key={id} />
         ))}
--- apps/web/components/app/dashboard/card-organizations.tsx
+++ apps/web/components/app/dashboard/card-organizations.tsx
@@ -1,20 +1,35 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@feedvote/ui'
 import { stringAvatar } from '@feedvote/utils'
+import { getProjectsOrganization } from '@lib/api/project'
+import { FolderKanbanIcon } from 'lucide-react'
 
+import { unstable_cache as cache } from 'next/cache'
 import Link from 'next/link'
 
-export const CardOrganization = ({ logo, name, slug }: { logo: string | null; name: string; slug: string }) => {
+const getProjects = cache(getProjectsOrganization, ['projects'], { tags: ['projects'] })
+
+export const CardOrganization = async ({ logo, name, slug }: { logo: string | null; name: string; slug: string }) => {
+  const projects = await getProjects(slug)
+
   return (
-    <section className="relative flex flex-col gap-5 rounded-md bg-card">
-      <header className="border-b p-5">
-        <Link href={`/${slug}/admin`} className="flex items-center gap-3">
-          <Avatar className="size-5">
-            <AvatarImage src={logo ?? '/_static/logo.svg'} className="size-5" />
-            <AvatarFallback>{stringAvatar(name)}</AvatarFallback>
-          </Avatar>
+    <section className="relative flex flex-col gap-6 rounded-md border bg-card px-5 pt-6 pb-5 transition-all hover:border-primary">
+      <header className="flex items-center gap-x-4">
+        <Avatar className="size-10">
+          <AvatarImage src={logo ?? '/_static/logo.svg'} className="size-10" />
+          <AvatarFallback>{stringAvatar(name)}</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col">
           <h2 className="font-bold">{name}</h2>
-        </Link>
+          <p className="font-medium text-muted-foreground">/{slug}</p>
+        </div>
       </header>
+      <footer className="flex gap-2 px-2">
+        <div className="flex items-center gap-2 font-medium">
+          <FolderKanbanIcon className="size-5" />
+          Projects {projects.data?.length}
+        </div>
+      </footer>
+      <Link href={`/${slug}/admin`} className="absolute inset-0 z-10 block" />
     </section>
   )
 }
